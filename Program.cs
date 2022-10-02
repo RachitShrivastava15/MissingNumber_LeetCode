174e716b415fdf2d2f17efdebcfe874cea59abc8
RachitShrivastava <rachitshrivastava15@gmail.com>
2022-10-02 00:33:47 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace MisingNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = {3,0,1};
+            int sum = nums.Length * (nums.Length + 1) / 2;
+		
+            for(int i = 0; i < nums.Length; i++) {
+                sum -= nums[i];
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
